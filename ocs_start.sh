a74b7afec2ea660b3088379fa77be1d6bc551eff
uas <uas@uas-Inspiron-8200.(none)>
2014-11-09 15:19:40 -0500

    Initial Occupancy commit

---
+++ ocs_start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###############################################################################
+# Occupancy Service - Start script
+# Unallocated Space
+#   Written by Pax - 2014-11-08
+#   filename = ocs_start.sh
+#
+#   Startup instructions:
+#   run from cron to start the service
+#       add `@restart /opt/ocs/ocs_start.sh` to cron file
+#
+#   Must have access to /opt/ocs/ to run ocs.sh
+###############################################################################
+
+while `/opt/ocs/ocs.sh`; do
+    echo "Occupancy Service has crashed. Restarting in 10 seconds."
+    sleep 10
+done
+echo "Occupancy Service has exited cleanly. This shouldn't happen."
